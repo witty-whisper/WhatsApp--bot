1fcafa071d008ba768e4abffea63f66f6ab5e14d
Marvellous Ebusalome Festus <woodsjeidah@gmail.com>
2026-05-01 12:38:01 +0100

    Refactor verify function for clarity and status handling
    Refactor the verify function to improve readability and return a 403 status code for invalid tokens.

--- app.py
+++ app.py
@@ -6,10 +6,13 @@ app = Flask(__name__)
 VERIFY_TOKEN = "marvellous2026"
 WHATSAPP_TOKEN = "EAActT6o6XZA4BRYdZAsDCCLoZAD8uKZCWrAFr4iGZBngQUYbXJYapTagz8rGJz7ZBldnHZARSdGTO6m2sR4ZAJbDuoKXYuDQOI6F0Mo82JTbmtZA6BvyOvDBDGFsaDQYoVLbZBG5tLyAjd5Sh7ZA1TIMa486NscUzmT60g0TEdOUqJNZBQZAlISS0wqnmUSpwZAiRXQmBAviVktNzLGG5n7HkvI2t5bjhpj7pqTsLhKZCSZAuDnQmG54zX5CuSMytxRBkNhD3UpDMYD9fZBnQwUnwC6ou6cDGtgZDZD" 
 PHONE_NUMBER_ID = "1049313231604664"
-
-@app.route('/webhook', methods=['GET'])
 def verify():
-    return request.args.get('hub.challenge') if request.args.get('hub.verify_token') == VERIFY_TOKEN else 'Invalid'
+    verify_token = request.args.get('hub.verify_token')
+    challenge = request.args.get('hub.challenge')
+    if verify_token == VERIFY_TOKEN:
+        return challenge
+    return "Invalid", 403
+@app
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
